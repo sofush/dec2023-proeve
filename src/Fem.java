920577a261e8dbcad0aea5801f8e2a65b046c9d4
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2023-12-20 09:26:34 +0100

    feat: tilføj Fem

---
+++ src/Fem.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class Fem {
+    public static int antalForekomster(char[] karTabel, char kar) {
+        int antal = 0;
+
+        for (char k : karTabel) {
+            if (k == kar) antal++;
+        }
+
+        return antal;
+    }
+
+    public static void main(String[] args) {
+        char[] arr = new char[5];
+        arr[0] = 'a';
+        arr[1] = 'b';
+        arr[2] = 'a';
+        arr[3] = 'c';
+        arr[4] = 'b';
+        System.out.printf("Forekomster af 'a': %d%n", antalForekomster(arr, 'a'));
+        System.out.printf("Forekomster af 'b': %d%n", antalForekomster(arr, 'b'));
+        System.out.printf("Forekomster af 'c': %d%n", antalForekomster(arr, 'c'));
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Fire.main(args);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        Fem.main(args);
     }
 }
